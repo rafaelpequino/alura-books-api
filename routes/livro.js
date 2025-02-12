6d39e666041e67e105dc9b4bc584960a60ef7add
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-12 16:09:10 -0300

    Adiciona métodos adicionais na rota livro

--- routes/livro.js
+++ routes/livro.js
@@ -3,7 +3,24 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.send("Hello world")
+    try {
+        res.send("Hello world")
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+})
+
+router.post("/", (req, res) => {
+    res.send("Você fez uma requisição post")
+})
+
+router.patch("/", (req, res) => {
+    res.send("Você fez uma requisição patch")
+})
+
+router.delete("/", (req, res) => {
+    res.send("Você fez uma requisição delete")
 })
 
 module.exports = router
