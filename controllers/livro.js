4372b86ff5f4dd4a45ba06551a5259b0e7d594ba
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-18 09:14:55 -0300

    Adiciona get com filtro de id

--- controllers/livro.js
+++ controllers/livro.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const { getTodosLivros } = require('../services/livro')
+const { getTodosLivros, getLivroPorId } = require('../services/livro')
 
 function getLivros(req, res) {
     try {
@@ -11,6 +11,18 @@ function getLivros(req, res) {
     }
 }
 
+function getLivro(req, res) {
+    try {
+        const id = req.params.id
+        const livro = getLivroPorId(id)
+        res.send(livro)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
 module.exports = {
-    getLivros
+    getLivros,
+    getLivro
 }
--- services/livro.js
+++ services/livro.js
@@ -4,6 +4,14 @@ function getTodosLivros () {
     return JSON.parse( fs.readFileSync("livros.json") )
 }
 
+function getLivroPorId (id) {
+    const livros = JSON.parse( fs.readFileSync("livros.json") )
+    const livro = livros.filter( livro => livro.id === id )
+    return livro
+}
+
+
 module.exports = {
-    getTodosLivros
+    getTodosLivros,
+    getLivroPorId
 }
